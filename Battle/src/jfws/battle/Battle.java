50a2db66fe8e934cf041d16e6f6b31d22797408b
Orchaldir <orchaldir@gmail.com>
2015-03-22 08:59:31 +0100

    Added project Battle.

---
+++ Battle/src/jfws/battle/Battle.java
@@ -0,0 +1,12 @@
+package jfws.battle;
+
+public class Battle
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Battle Prototype");
+		System.out.println("----------------\n");
+	}
+	
+}
