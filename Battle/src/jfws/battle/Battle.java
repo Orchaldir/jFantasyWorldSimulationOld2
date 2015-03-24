b41997bab4e375bb4d1f51bbe53533ce22ec1e71
Orchaldir <orchaldir@gmail.com>
2015-03-24 21:06:11 +0100

    Added processInput() & processMove().

--- Battle/src/jfws/battle/Battle.java
+++ Battle/src/jfws/battle/Battle.java
@@ -1,22 +1,31 @@
 package jfws.battle;
 
+import java.util.Scanner;
 import jfws.gameplay.war.battlefield.*;
 import jfws.gameplay.war.unit.Unit;
 
 public class Battle
 {
+	public static Scanner input_ = new Scanner(System.in);
+	public static BattlefieldMap battlefield_ = new BattlefieldMap(5, 2);
+	public static int cell_size_ = 3;
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Battle Prototype");
 		System.out.println("----------------\n");
 		
-		BattlefieldMap battlefield = new BattlefieldMap(5, 2);
 		Unit unit = new Unit("HI1");
 		
-		battlefield.addUnit(unit, 2, 0);
+		battlefield_.addUnit(unit, 2, 0);
 		
-		printBattlefield(battlefield, 3);
+		printBattlefield(battlefield_, cell_size_);
+		
+		while(true)
+		{
+			processInput(unit);
+			printBattlefield(battlefield_, cell_size_);
+		}
 	}
 	
 	public static void printBattlefield(BattlefieldMap battlefield, int cell_size)
@@ -51,4 +60,60 @@ public class Battle
 		}
 	}
 	
+	public static boolean processInput(Unit unit)
+	{
+		System.out.println("");
+		System.out.print(unit.getName() + "'s action: ");
+		
+		String command = input_.nextLine();
+		String[] parts = command.split(" ");
+		
+		if(parts[0].equals("move"))
+		{
+			return processMove(unit, parts);
+		}
+		
+		System.err.println("Unknown command!");
+		
+		return false;
+	}
+	
+	public static boolean processMove(Unit unit, String[] parts)
+	{
+		if(parts.length != 2)
+		{
+			System.err.println("Move command is invalid!");
+			return false;
+		}
+
+		String dir_name_ = parts[1];
+		Direction dir;
+
+		switch(dir_name_)
+		{
+			case "north":
+				dir = Direction.NORTH;
+				break;
+			case "east":
+				dir = Direction.EAST;
+				break;
+			case "south":
+				dir = Direction.SOUTH;
+				break;
+			case "west":
+				dir = Direction.WEST;
+				break;
+			default:
+				System.err.println("Invalid Direction!");
+				return false;
+		}
+
+		if(!battlefield_.moveUnit(unit, dir))
+		{
+			System.err.println("Could not move!");
+			return false;
+		}
+
+		return true;
+	}
 }
